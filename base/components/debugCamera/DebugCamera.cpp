4dfe9bb1cad00380daa0e3ae4cda792dffa1686f
Y04Kato <k022g0046@g.neec.ac.jp>
2023-10-17 15:41:57 +0900

    Camera操作をキーでできるように設定

--- base/components/debugCamera/DebugCamera.cpp
+++ base/components/debugCamera/DebugCamera.cpp
@@ -9,9 +9,24 @@ void DebugCamera::initialize() {
 	viewProjection_.Initialize();
 	viewProjection_.translation_ = { 0,0,-30 };
 	viewProjection_.rotation_ = { 0,0,0 };
+
+	input_ = Input::GetInstance();
 }
 
 void DebugCamera::Update() {
+	if (input_->PressKey(DIK_UPARROW)) {
+		viewProjection_.rotation_.num[0] -= 0.05f;
+	}
+	if (input_->PressKey(DIK_DOWNARROW)) {
+		viewProjection_.rotation_.num[0] += 0.05f;
+	}
+	if (input_->PressKey(DIK_RIGHTARROW)) {
+		viewProjection_.rotation_.num[1] += 0.05f;
+	}
+	if (input_->PressKey(DIK_LEFTARROW)) {
+		viewProjection_.rotation_.num[1] -= 0.05f;
+	}
+
 	ImGui::Begin("DebugCamera");
 	ImGui::SliderFloat3("rotation", viewProjection_.rotation_.num, -20, 20);
 	ImGui::SliderFloat3("translation", viewProjection_.translation_.num, -20, 20);
